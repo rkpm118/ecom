0b9f90a363d287259793a613b6348382c56d3ecb
jspmern <jspmern@gmail.com>
2023-05-25 11:47:09 +0530

    first commit

---
+++ src/component/Navbar.js
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom";
+import {BsCart } from "@react-icons/all-files/fa/FaBeer";
+function Navbar()
+{
+    return(
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <NavLink className="navbar-brand" to="/">Navbar</NavLink>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <NavLink className="nav-link active" aria-current="page" href="#">Home</NavLink>
+        <NavLink className="nav-link" to="/">Features</NavLink>
+        <NavLink className="nav-link" to="/">Pricing</NavLink>
+        <NavLink><BsCart/></NavLink>
+      </div>
+    </div>
+  </div>
+</nav>
+        </>
+    )
+}
+export default Navbar;
