dd09c6943d0059fc54e1547f0c9eba3c88a9d073
jspmern <jspmern@gmail.com>
2023-05-26 11:46:04 +0530

    navstyling

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
  
 import { BsCart} from "react-icons/bs"
+import Button from "./CommonTemplate/Button";
 function Navbar()
 {
     return(
@@ -12,11 +13,16 @@ function Navbar()
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav  flex-grow-1 justify-content-end ">
-        <NavLink className="nav-link active" aria-current="page" href="#">Home</NavLink>
-        <NavLink className="nav-link" to="/">Features</NavLink>
-        <NavLink className="nav-link" to="/">Pricing</NavLink>
-        <NavLink><BsCart/></NavLink>
+      <div className="navbar-nav  flex-grow-1 justify-content-end  align-items-center">
+        <NavLink className="nav-link active" aria-current="page" href="#">HOME</NavLink>
+        <NavLink className="nav-link" to="/">ABOUT</NavLink>
+        <NavLink className="nav-link" to="/">PRODUCTS</NavLink>
+        <NavLink className="nav-link" to="/">CONTACT</NavLink>
+        <NavLink className="nav-link" to="/"><Button btnName="LOG IN"/></NavLink>
+        <NavLink className="position-relative me-2"><BsCart  style={{fontSize:"25px"}} />   <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+    99+
+    <span class="visually-hidden">unread messages</span>
+  </span></NavLink>
       </div>
     </div>
   </div>
