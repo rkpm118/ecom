ecc8de9daf78e3c0f5f9e9a662d2655c6a1b79d8
jhautsav <utsavjha2020@gmail.com>
2023-06-23 23:59:43 +0530

    logo name is change in this commit inside navbar again

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,34 +1,56 @@
 import { NavLink } from "react-router-dom";
- 
-import { BsCart} from "react-icons/bs"
+
+import { BsCart } from "react-icons/bs";
 import Button from "./CommonTemplate/Button";
- 
-function Navbar()
-{
-    return(
-        <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <NavLink className="navbar-brand" to="/">Utsav Store</NavLink>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav  flex-grow-1 justify-content-end  align-items-center">
-        <NavLink className="nav-link active" aria-current="page" to='/'>HOME</NavLink>
-        <NavLink className="nav-link" to="/about">ABOUT</NavLink>
-        <NavLink className="nav-link" to="/products">PRODUCTS</NavLink>
-        <NavLink className="nav-link" to="/contact">CONTACT</NavLink>
-        <NavLink className="nav-link" to="/"><Button btnName="LOG IN"/></NavLink>
-        <NavLink to='/cart' className="position-relative me-2"><BsCart  style={{fontSize:"25px"}} />   <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-    99+
-    <span class="visually-hidden">unread messages</span>
-  </span></NavLink>
-      </div>
-    </div>
-  </div>
-</nav>
-        </>
-    )
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/">
+            Utsav Store
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav  flex-grow-1 justify-content-end  align-items-center">
+              <NavLink className="nav-link active" aria-current="page" to="/">
+                HOME
+              </NavLink>
+              <NavLink className="nav-link" to="/about">
+                ABOUT
+              </NavLink>
+              <NavLink className="nav-link" to="/products">
+                PRODUCTS
+              </NavLink>
+              <NavLink className="nav-link" to="/contact">
+                CONTACT
+              </NavLink>
+              <NavLink className="nav-link" to="/">
+                <Button btnName="LOG IN" />
+              </NavLink>
+              <NavLink to="/cart" className="position-relative me-2">
+                <BsCart style={{ fontSize: "25px" }} />{" "}
+                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                  99+
+                  <span class="visually-hidden">unread messages</span>
+                </span>
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 export default Navbar;
