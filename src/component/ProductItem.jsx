8e92733de28ea9427ceface39973ce79dc27b5cf
jspmern <jspmern@gmail.com>
2023-07-07 12:08:09 +0530

    fixed mistake in productItem and this is the updated code for the product Item

--- src/component/ProductItem.jsx
+++ src/component/ProductItem.jsx
@@ -5,16 +5,13 @@ import ListView from './ListView'
 
 function ProductItem() {
   const { filter_products, setGridView, grid_view } = useFilterContext()
-  console.log('this is the filterproduct and this is the for allproduct form product list ', filter_products, setGridView, grid_view)
   if (grid_view) {
     return <GridView products={filter_products} />
   }
   if (grid_view === false) {
     return <ListView products={filter_products} />
   }
-  return (
-    <div>ProductItem</div>
-  )
+
 }
 
 export default ProductItem
